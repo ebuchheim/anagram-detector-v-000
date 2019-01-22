32e6e7ccf21fca8cc0e550eef76c1c4813e9c4a0
ebuchheim <emma.buchheim@gmail.com>
2019-01-22 09:05:54 -0800

    add anagram detector logic

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_options)
+        array_to_match = @word.split("")
+        found_it = []
+        array_of_options.each do |possible_match|
+            poss_match_array = possible_match.split("")
+            if poss_match_array.sort == array_to_match.sort
+                found_it << possible_match
+            end
+        end
+        found_it
+    end
+
+end
